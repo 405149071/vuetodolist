7f7395058d0b0cbfc200cdcc89e83b861dcf0c3a
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-08-16 18:32:47 +0800

    父子组件互相主动调用对方的数据和方法

---
+++ src/components/Header2.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="header">
+    <h2>这是头部2</h2>
+    <button @click="callParent()">调用父组件的数据和方法</button>
+  </div>
+</template>
+
+<script>
+// 子组件主动调用父组件的数据和方法
+// this.$parent.msg
+// this.$parent.run()
+export default {
+  data() {
+    return {
+      msg: "子组件msg"
+    };
+  },
+
+  mounted() {
+    console.log("这是头部组件mounted");
+  },
+  methods: {
+    run() {
+      alert("我是子组件的run方法");
+    },
+    callParent() {
+      alert(this.$parent.msg);
+      this.$parent.run();
+    }
+  }
+};
+</script>
+
+<style scoped>
+#header {
+  background-color: black;
+  color: #999;
+}
+</style>
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <v-header />
+    <v-header2 ref="header2" />
     <h2>这是新闻页面</h2>
     <ul>
       <li>1111111</li>
       <li>2222</li>
       <li>3333</li>
     </ul>
+    <button @click="callsub()">调用子组件数据和方法</button>
   </div>
 </template>
 
 <script>
-import Header from "../components/Header";
+// 父组件主动调用子组件的数据和方法
+// 1, <v-header ref="header" />
+// 2 this.$refs.header.msg // 数据
+// this.$refs.header.run() // 方法
+import Header2 from "../components/Header2";
 
 export default {
+  data() {
+    return {
+      msg: "我是父组件msg"
+    };
+  },
   components: {
-    "v-header": Header
+    "v-header2": Header2
+  },
+  methods: {
+    run() {
+      alert("我是父组件的run");
+    },
+    callsub() {
+      alert(this.$refs.header2.msg); // 数据
+      this.$refs.header2.run();
+    }
   }
 };
 </script>

1c9cac1db8c6358ad98fe14ec16278874568cf9a
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-08-16 11:14:21 +0800

    基础的todolist方法

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view />-->
+    <input type="text" v-model="todo" />
+    <button @click="add">+增加</button>
+    <br />
+    <hr />
+    <ul>
+      <li v-for="(item,key) in list">
+        {{key}}--{{item}} -------
+        <button @click="remove(key)">删除</button>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: "home",
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      todo: "",
+      list: []
+    };
+  },
+  methods: {
+    add() {
+      this.list.push(this.todo);
+      this.todo = "";
+    },
+    remove(index) {
+      this.list.splice(index, 1);
     }
   }
-}
+};
+</script>
+
+<style lang="less">
+// #app {
+//   font-family: "Avenir", Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
